cb88f672ef2dbf104d56c88d6afe7d8e75885b24
Ravi Ranjan <ravi.ranjan@pw.live>
2024-12-21 10:52:14 +0530

    Initial Commit

---
+++ Pro11/Pro11App.swift
@@ -0,0 +1,17 @@
+//
+//  Pro11App.swift
+//  Pro11
+//
+//  Created by Ravi Ranjan on 21/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Pro11App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
